c83abfdee502d0ff3234b5a056a84d2e56a7773d
Greg <gregoil@walla.co.il>
2019-09-01 15:33:42 +0300

    initiated rotest environment

--- pytest_rotest.py
+++ pytest_rotest.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-import ipdb
 import six
 import sys
+import json
 import argparse
 from itertools import chain
 
-import pytest
-import pkg_resources
 from attrdict import AttrDict
 from rotest.common import core_log
 from rotest.core.result.result import Result
+from rotest.cli.discover import is_test_class
+from rotest.core.models.run_data import RunData
 from rotest.core.abstract_test import AbstractTest
 from rotest.core.result.result import get_result_handlers
 from rotest.core import TestSuite, TestCase, TestFlow, TestBlock
@@ -18,6 +18,15 @@ from rotest.cli.client import parse_outputs_option, filter_valid_values
 from rotest.core.runner import (DEFAULT_CONFIG_PATH, parse_config_file,
                                 update_resource_requests,
                                 parse_resource_identifiers)
+
+
+class RotestRunContext(object):
+    CONFIG = None
+    RESULT = None
+    RUN_DATA = None
+    MAIN_TEST = None
+    RESOURCE_MANAGER = None
+    COLLECTED_CLASSES = []
 
 
 class OutputHandlersParseAction(argparse.Action):
@@ -46,42 +55,78 @@ def pytest_addoption(parser):
     )
 
 
-def _pytest_configure(config):
-    print('aaaaaaaaa')
-    class AlmightySuite(TestSuite):
-        components = []
+def pytest_runtest_setup(item):
+    RotestRunContext.RESULT.startTest(item)
 
-    session.rotest_config = AttrDict(chain(
-        six.iteritems(parse_config_file(DEFAULT_CONFIG_PATH)),
-        six.iteritems(parse_config_file(config.option.config_path)),
-        filter_valid_values({'outputs': config.option.outputs})
-        #filter_valid_values(vars(config)),
-    ))
 
-    session.rotest_result = Result(stream=sys.stdout,
-                                   outputs=session.rotest_config.outputs)
-    session.rotest_manager = ClientResourceManager(logger=core_log)
+def pytest_runtest_call(item):
+    RotestRunContext.RESULT.setupFinished(item)
+
+
+def pytest_runtest_teardown(item):
+    RotestRunContext.RESULT.startTeardown(item)
 
 
 def pytest_runtest_setup(item):
-    if issubclass(item.parent._obj, AbstractTest):
+    if issubclass(item.parent.obj, AbstractTest):
+        item.result = RotestRunContext.RESULT
         item._is_client_local = False
-        if (item._is_client_local and
-                item.resource_manager.is_connected()):
-            item.resource_manager.disconnect()
         item.resource_manager = item.session.rotest_manager
 
 
-
-def pytest_pycollect_makeitem(collector, name, obj):
-    if isinstance(obj, type) and \
-            issubclass(obj, (TestSuite, TestCase, TestFlow, TestBlock)):
-        print(collector, name, obj)
-
-
 def pytest_collection_modifyitems(session, config, items):
+    rotest_items = []
     for item in items[:]:
-        if item.parent._obj in (TestSuite, TestCase, TestFlow, TestBlock):
-            items.remove(item)
+        if issubclass(item.parent.obj, (TestSuite, TestCase, TestFlow, TestBlock)):
+            if is_test_class(item.parent.obj):
+                import ipdb; ipdb.set_trace()
+                rotest_items.append(item)
+
+            else:
+                items.remove(item)
+
+    if rotest_items:
+        RotestRunContext.CONFIG = AttrDict(chain(
+            six.iteritems(parse_config_file(DEFAULT_CONFIG_PATH)),
+            six.iteritems(parse_config_file(config.option.config_path)),
+            filter_valid_values({'outputs': config.option.outputs})
+            # filter_valid_values(vars(config)),
+        ))
+
+        RotestRunContext.RUN_DATA = RunData(
+            config=json.dumps(RotestRunContext.CONFIG))
+        RotestRunContext.RESOURCE_MANAGER = ClientResourceManager(logger=core_log)
+
+        config.__dict__.update(RotestRunContext.CONFIG)
+
+        class AlmightySuite(TestSuite):
+            components = [TestCase]
+
+        RotestRunContext.MAIN_TEST = AlmightySuite(
+            run_data=RotestRunContext.RUN_DATA,
+            config=config,
+            skip_init=RotestRunContext.CONFIG.skip_init,
+            save_state=RotestRunContext.CONFIG.save_state,
+            enable_debug=RotestRunContext.CONFIG.debug,
+            resource_manager=RotestRunContext.RESOURCE_MANAGER)
+
+        RotestRunContext.MAIN_TEST._tests = rotest_items
+
+        RotestRunContext.RUN_DATA.main_test = RotestRunContext.MAIN_TEST.data
+
+        RotestRunContext.RESULT = Result(stream=sys.stdout,
+                                         outputs=RotestRunContext.CONFIG.outputs,
+                                         main_test=RotestRunContext.MAIN_TEST)
 
     import ipdb; ipdb.set_trace()
+    RotestRunContext.RESULT.startTestRun()
+
+def pytest_sessionfinish(session, exitstatus):
+    """Finalize the test runner.
+
+    * Removes duplicated test DB entries.
+    """
+    RotestRunContext.RESULT.stopTestRun()
+    if RotestRunContext.RESOURCE_MANAGER is not None:
+        core_log.debug("Closing the resource manager")
+        RotestRunContext.RESOURCE_MANAGER.disconnect()
